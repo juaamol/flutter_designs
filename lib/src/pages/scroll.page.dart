63ad65f32ff656a574a1bd1581d29c54edc37d49
juaamol <jambulud@gmail.com>
2019-06-08 14:31:03 +0200

    Weather page

--- lib/src/pages/scroll.page.dart
+++ lib/src/pages/scroll.page.dart
@@ -15,14 +15,58 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _page1() {
-    return Center(
-      child: Text('Page 1'),
+    return Stack(
+      children: <Widget>[_backgroundColor(), _backgroundImage(), _texts()],
+    );
+  }
+
+  Widget _texts() {
+    final textStyle = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 20.0,
+          ),
+          Text('11°', style: textStyle),
+          Text(
+            'Wednesday',
+            style: textStyle,
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Icon(
+            Icons.keyboard_arrow_down,
+            size: 70.0,
+            color: Colors.white,
+          )
+        ],
+      ),
     );
   }
 
   Widget _page2() {
     return Center(
       child: Text('Page 2'),
+    );
+  }
+
+  Widget _backgroundColor() {
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Color.fromRGBO(108, 192, 218, 0.5));
+  }
+
+  Widget _backgroundImage() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
     );
   }
 }
